1520834422c02e44d72d9b20921c3b93a5f1a9e7
umerfareed72 <60812809+umerfareed72@users.noreply.github.com>
2024-03-03 11:13:46 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {LanguageContextProvider} from './src/LanguageContext';
+import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  // console.log('Message handled in the background!', remoteMessage);
+});
+
+AppRegistry.registerComponent(appName, () => LanguageContextProvider);
